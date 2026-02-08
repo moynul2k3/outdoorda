3b0497ee92a4b5f3b587a7adec0b4d31b222423b
jahir <jahirulislam92134@gmail.com>
2026-02-08 15:12:54 +0600

    add firebase sdk

--- app/utils/firebase_push.py
+++ app/utils/firebase_push.py
@@ -3,6 +3,6 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate('app/quiklerider-firebase-sdk.json')
+cred = credentials.Certificate('app/outdoorda-firebase-sdk.json')
 firebase_admin.initialize_app(cred)
 print(f"Firebase initialized {firebase_admin.get_app().name}")
